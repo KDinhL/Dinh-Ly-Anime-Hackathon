98dd721e1b7eb1dd4acfe0a2a3eb2943adae5873
AlChi1997 <alex.chi1997@hotmail.com>
2023-08-09 19:33:54 -0400

    Started a basis for our anime web application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
-function App() {
-  return (
+
+const App =() =>{
+  const [anime, setAnime] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://kitsu.io/api/edge/anime?filter[text]=one-piece')
+    .then((response) => {
+      setAnime(response.data.data);
+    })
+  }, [])
+
+  console.log(anime);
+  
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Anime</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/anime" element={<AnimeList />}/>
+          <Route path="/anime/:id" element={<Anime />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
